3d1af27dbb605a6d2b170e18f4fb9f513d1e9f9d
Matthew Cory <matthewcory1@gmail.com>
2021-10-06 11:33:40 -0600

    Hello world implementation for testing purposes.

--- sloth-cli/src/main/kotlin/sloth/app/App.kt
+++ sloth-cli/src/main/kotlin/sloth/app/App.kt
@@ -1,5 +1,11 @@
 package sloth.app
 
-fun main() {
-    
+import com.github.ajalt.clikt.core.CliktCommand
+
+class Hello : CliktCommand() {
+    override fun run() {
+        echo("Hello World!")
+    }
 }
+
+fun main(args: Array<String>) = Hello().main(args)
